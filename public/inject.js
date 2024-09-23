03ef148a42818057136c0ff112144a37c3841500
MonteCrypto <theofcourse01@gmail.com>
2024-09-23 14:02:51 +0200

    Change connector name and few fix

--- public/inject.js
+++ public/inject.js
@@ -1,22 +1,22 @@
 (function () {
 
-  window.pipe = window.pipe || {};
+  windows.pipeline = windows.pipeline || {};
 
-  window.address = window?.pipe?.address || null;
+  window.address = window?.pipeline?.address || null;
 
-  window.pipe.connect = function () {
+  windows.pipeline.connect = function () {
     const event = new CustomEvent("connect");
     window.dispatchEvent(event);
   };
 
-  window.pipe.sendTransaction = function (recipient, amount, psbt) {
+  windows.pipeline.sendTransaction = function (recipient, amount, psbt) {
     const event = new CustomEvent("sendTransaction", {
       detail: { recipient, amount, psbt },
     });
     window.dispatchEvent(event);
   };
 
-  window.pipe.signPsbt = function (psbtBase64) {
+  windows.pipeline.signPsbt = function (psbtBase64) {
     const event = new CustomEvent("signPsbt", {
       detail: { psbtBase64 },
     });
@@ -28,8 +28,8 @@
     (result) => {
       if (result.data.action === "finishConnect") {
         if (result.data.address) {
-          window.pipe.address = result.data.address;
-          window.pipe.pubInternalKey = result.data.pubInternalKey;
+          windows.pipeline.address = result.data.address;
+          windows.pipeline.pubInternalKey = result.data.pubInternalKey;
 
           const connectEvent = new CustomEvent("connectToSite", {
             detail: { connectedWallet: result.data.address, pubInternalKey: result.data.pubInternalKey },
