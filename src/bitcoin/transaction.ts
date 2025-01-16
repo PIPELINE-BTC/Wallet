3060ad3d7df5272dfb152ed56b441921041f7527
MonteCrypto <theofcourse01@gmail.com>
2025-01-16 15:06:23 +0100

    Allow all BTC address as output and cleanup

--- src/bitcoin/transaction.ts
+++ src/bitcoin/transaction.ts
@@ -4,7 +4,7 @@
 import BIP32Factory from "bip32";
 import * as ecc from "@bitcoin-js/tiny-secp256k1-asmjs";
 import * as bip39 from "bip39";
-import { toXOnly } from "./utils";
+import { toXOnly, getAddressType, estimateOutputVbytes, BASE_OVERHEAD, INPUT_SIZES, OUTPUT_SIZES } from "./utils";
 import * as bitcoin from 'bitcoinjs-lib';
 import ECPairFactory from "ecpair";
 
@@ -276,6 +276,7 @@ export const sendSats = async (
     network,
   });
 
+
   const tweakedChildNode = childNode.tweak(
     bitcoin.crypto.taggedHash("TapTweak", childNodeXOnlyPubkey)
   );
@@ -320,13 +321,18 @@ export const sendSats = async (
   const availableInput = [];
   const usedUtxos = new Set();
   const selectedUtxos = [];
-  const additionalFeeForInput = 57 * txfee;
+  const additionalFeeForInput = INPUT_SIZES.p2tr * txfee;
   let totalSelectedValue = 0;
   let totalAvailableValue = 0;
 
   let amountToBeSentSat = Math.floor(amountBtc * 1e8);
 
-  let estimatedFee = (111 - 57) * txfee;
+  const senderAddressType = getAddressType(to, network);
+  const outputSize = estimateOutputVbytes(senderAddressType);
+  
+  const inputSize = INPUT_SIZES.p2tr;
+
+  let estimatedFee = (BASE_OVERHEAD + inputSize + outputSize) * txfee;
 
   if (amountToBeSentSat < 0) {
     throw new Error("Not enough BTC to cover fees");
@@ -784,7 +790,12 @@ export const sendTransferTransaction = async (
     data = [...data, ...changeData];
   }
 
-  const baseOutput = 3;
+  const baseOutput = [
+    { type: getAddressType(to), count: 1 },
+    ...(selectedToken.cumulativeAmount > transferAmount ? [{ type: "p2tr", count: 1 }] : []),
+    { type: "p2tr", count: 1 },
+  ];
+  
   const baseInput = 2;
   const tapscriptBitcoin = bitcoin.script.compile(data);
 
